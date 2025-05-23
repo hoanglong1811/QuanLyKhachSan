7ed06f982b450d57703f6bf80c25f8ebb2fcff65
nguyenhoanghuy <nguyenhuy251201@gmail.com>
2025-05-23 10:48:46 +0700

    update

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
+
 const app = createApp(App);
 app.use(router);
 app.mount('#app');
--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,22 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomePage from '../pages/HomePage.vue';
-import AboutUs from '../pages/AboutUs.vue';
-import Services from '@/pages/Services.vue';
-import ContactUs from '@/pages/ContactUs.vue';
-
+import SignupPage from '../components/SignUpPage.vue';
+import CreateAccPage from '../components/CreateAccPage.vue';
+import ForgotPassPage from '../components/ForgotPassPage.vue';
+import OTPPage from '../components/OTPPage.vue';
+import ResetPass from '../components/ResetPassPage.vue';
 
 const routes = [
-    { path: '/', name: 'Home', component: HomePage },
-    { path: '/about', name: 'AboutUs', component: AboutUs },
-    { path: '/ser', name: 'Services', component: Services },
-    { path: '/contact', name: 'ContactUs', component: ContactUs },
-    
-
+  { path: '/login', component: SignupPage },
+  { path: '/create-account', component: CreateAccPage },
+  { path: '/forgot-password', component: ForgotPassPage },
+  { path: '/otp', component: OTPPage },
+  { path: '/reset-password', component: ResetPass },
+  { path: '/', redirect: '/create-account' }, // Default route
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
