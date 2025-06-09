40f4086ca34f8c50c0be0bc2ba419bd235aee901
Hoang Long <164651081+hoanglong1811@users.noreply.github.com>
2025-06-10 02:23:49 +0700

    update 10

--- back-end/obj/Debug/net8.0/back-end.AssemblyInfo.cs
+++ back-end/obj/Debug/net8.0/back-end.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back-end")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+540c1eb6c5b7cf2637a2fe9aeccb44e936b5f3c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93cc702397f6ed3010f4ae67a121712589a3843f")]
 [assembly: System.Reflection.AssemblyProductAttribute("back-end")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back-end")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
