465f7755961ba37b4bba488dabab4836e37d9ae8
Hoang Long <164651081+hoanglong1811@users.noreply.github.com>
2025-06-10 14:27:10 +0700

    update rener final

--- back-end/Program.cs
+++ back-end/Program.cs
@@ -73,12 +73,8 @@ builder.Services.AddAuthentication(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseCors("AllowVueApp");
